9f059745383cc8055f9ff772a407e1bf921df39b
jinyuttt <jinyu20072787@sina.com>
2026-06-29 01:05:28 +0800

    Delete temp_compare directory

--- temp_compare/fix_bom.ps1
+++
@@ -1,4 +0,0 @@
-$path=D:\code\rtklib_java\src\test\java\org\rtklib\java\RinexConversionTest.java
-$bytes=[System.IO.File]::ReadAllBytes($path)
-$new=[System.IO.File]::ReadAllBytes($path)
-Write-Host ($bytes.Length)
